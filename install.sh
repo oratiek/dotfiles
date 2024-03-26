9d0c8afcf9cc8a194547298b6fabf13e1eaa1eed
oratiek <oratiek522@gmail.com>
2024-03-26 12:42:03 +0900

    linuxbrewのインストールを追加

--- install.sh
+++ install.sh
@@ -58,3 +58,6 @@ fi
 
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# linux brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"

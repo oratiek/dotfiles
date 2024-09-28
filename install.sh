c1b25ce6b8ba3b61b94ba73de462b7a929bcf814
oratiek <oratiek522@gmail.com>
2024-09-28 23:32:29 +0900

    added installation of git

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ sudo apt install -y tmux
 sudo apt install -y zsh
 sudo apt install -y ibus-mozc mozc-utils-gui
 sudo apt install -y gnome-tweaks
+sudo apt install -y git
 
 # vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

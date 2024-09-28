0a8f60761a966ae35b8a4e74592df029e9b05f8f
oratiek <oratiek522@gmail.com>
2024-09-28 23:38:56 +0900

    added docker installation

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 sudo apt install -y curl
 sudo apt install -y tmux
 sudo apt install -y zsh
+sudo apt install -y vim-gtk
 sudo apt install -y ibus-mozc mozc-utils-gui
 sudo apt install -y gnome-tweaks
 sudo apt install -y git
@@ -15,3 +16,19 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 ln -s vim/.vimrc ~/.vimrc
 ln -s zsh/.zshrc ~/.zshrc
 ln -s tmux/.tmux.conf ~/.tmux.conf
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
